cfcf9249ac027e8595fa238770d5d3a1406ea1aa
RoyJoRub <rjrmph@gmail.com>
2020-08-08 13:17:31 -0700

    finished formating for continued lessons

--- src/algo_work.py
+++ src/algo_work.py
@@ -1,303 +1,174 @@
+# taken from https://towardsdatascience.com/10-algorithms-to-solve-before-your-python-coding-interview-feb74fb9bc27
+import collections
 
-
-# Given an integer, return the integer with reversed digits.
-# Note: The integer could be either positive or negative.
-
-def solution(x):
+def algo_01(x):
+    """
+    Given an integer, return the integer with reversed digits.
+    Note: The integer could be either positive or negative.
+    :param x:
+    :return:
+    """
     string = str(x)
-    
+
     if string[0] == '-':
-        return int('-'+string[:0:-1])
+        return int('-' + string[:0:-1])
     else:
         return int(string[::-1])
-    
-print(solution(-231))
-print(solution(345))
 
 
-
-
-
-# --------------------------
-
-
-
-
-
-
-
-# For a given sentence, return the average word length. 
-# Note: Remember to remove punctuation first.
-
-sentence1 = "Hi all, my name is Tom...I am originally from Australia."
-sentence2 = "I need to work very hard to learn more about algorithms in Python!"
-
-def solution(sentence):
+def algo_02(sentence):
+    """
+    For a given sentence, return the average word length.
+    Note: Remember to remove punctuation first.
+    :param sentence:
+    :return:
+    """
     for p in "!?',;.":
         sentence = sentence.replace(p, '')
     words = sentence.split()
-    return round(sum(len(word) for word in words)/len(words),2)
-    
-print(solution(sentence1))
-print(solution(sentence2))
+    return round(sum(len(word) for word in words) / len(words), 2)
 
 
+def algo_03a(num1, num2):
+    """
+    Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+    You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
+    Notes:
+    Both num1 and num2 contains only digits 0-9.
+    Both num1 and num2 does not contain any leading zero.
 
-
-
-
-
-#- ---------------------------
-
-
-
-
-
-# Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
-# You must not use any built-in BigInteger library or convert the inputs to integer directly.
-
-#Notes:
-#Both num1 and num2 contains only digits 0-9.
-#Both num1 and num2 does not contain any leading zero.
-
-num1 = '364'
-num2 = '1836'
-
-# Approach 1: 
-def solution(num1,num2):
+    :param num1, num2:
+    :return:
+    """
     eval(num1) + eval(num2)
     return str(eval(num1) + eval(num2))
-            
-print(solution(num1,num2))
 
-#Approach2 
-#Given a string of length one, the ord() function returns an integer representing the Unicode code point of the character 
-#when the argument is a unicode object, or the value of the byte when the argument is an 8-bit string.
 
-def solution(num1, num2):
+def algo_03b(num1, num2):
+    """
+    Given a string of length one, the ord() function returns an integer representing the Unicode code point of the character
+    when the argument is a unicode object, or the value of the byte when the argument is an 8-bit string.
+
+    :param num1, num2:
+    :return:
+    """
     n1, n2 = 0, 0
-    m1, m2 = 10**(len(num1)-1), 10**(len(num2)-1)
+    m1, m2 = 10 ** (len(num1) - 1), 10 ** (len(num2) - 1)
 
     for i in num1:
-        n1 += (ord(i) - ord("0")) * m1 
-        m1 = m1//10        
+        n1 += (ord(i) - ord("0")) * m1
+        m1 = m1 // 10
 
     for i in num2:
         n2 += (ord(i) - ord("0")) * m2
-        m2 = m2//10
+        m2 = m2 // 10
 
     return str(n1 + n2)
-print(solution(num1, num2))
 
 
-
-# ----------------------------
-
-
-
-
-
-# Given a string, find the first non-repeating character in it and return its index. 
-# If it doesn't exist, return -1. # Note: all the input strings are already lowercase.
-
-#Approach 1
-def solution(s):
+def algo_04a(s):
+"""
+Given a string, find the first non-repeating character in it and return its index.
+If it doesn't exist, return -1. # Note: all the input strings are already lowercase.
+"""
     frequency = {}
     for i in s:
         if i not in frequency:
             frequency[i] = 1
         else:
-            frequency[i] +=1
+            frequency[i] += 1
     for i in range(len(s)):
         if frequency[s[i]] == 1:
             return i
     return -1
 
-print(solution('alphabet'))
-print(solution('barbados'))
-print(solution('crunchy'))
 
-print('###')
+# Approach 2
+# import collections - at top of page and on test file
 
-#Approach 2
-import collections
-
-def solution(s):
-    # build hash map : character and how often it appears
-    count = collections.Counter(s) # <-- gives back a dictionary with words occurrence count 
-                                         #Counter({'l': 1, 'e': 3, 't': 1, 'c': 1, 'o': 1, 'd': 1})
-    # find the index
+def algo_04b(s):
+    count = collections.Counter(s)
     for idx, ch in enumerate(s):
         if count[ch] == 1:
-            return idx     
+            return idx
     return -1
 
-print(solution('alphabet'))
-print(solution('barbados'))
-print(solution('crunchy'))
 
-
-
-
-# ------------------------
-
-
-
-
-
-
-# Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
-# The string will only contain lowercase characters a-z.
-
-s = 'radkar'
-def solution(s):
+def algo_05(s):
+"""
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+The string will only contain lowercase characters a-z.
+"""
     for i in range(len(s)):
-        t = s[:i] + s[i+1:]
+        t = s[:i] + s[i + 1:]
         if t == t[::-1]: return True
 
     return s == s[::-1]
-  
-solution(s)
 
 
+def algo_06(nums):
+"""
+Given an array of integers, determine whether the array is monotonic or not.
+"""
+    return (all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)) or
+            all(nums[i] >= nums[i + 1] for i in range(len(nums) - 1)))
 
 
-
-
-# ---------------------------
-
-
-
-
-
-
-
-# Given an array of integers, determine whether the array is monotonic or not.
-A = [6, 5, 4, 4] 
-B = [1,1,1,3,3,4,3,2,4,2]
-C = [1,1,2,3,7]
-
-def solution(nums): 
-    return (all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)) or 
-            all(nums[i] >= nums[i + 1] for i in range(len(nums) - 1))) 
-  
-print(solution(A)) 
-print(solution(B)) 
-print(solution(C)) 
-
-
-
-
-
-
-
-
-# ------------------------
-
-
-
-
-
-
-
-#Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of 
-#the non-zero elements.
-
-array1 = [0,1,0,3,12]
-array2 = [1,7,0,0,8,0,10,12,0,4]
-
-def solution(nums):
+def algo_07(nums):
+"""
+Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of
+the non-zero elements.
+"""
     for i in nums:
         if 0 in nums:
             nums.remove(0)
             nums.append(0)
     return nums
 
-solution(array1)
-solution(array2)
 
-
-
-
-
-
-
-# ------------------------
-
-
-
-
-
-
-
-# Given an array containing None values fill in the None values with most recent 
-# non None value in the array 
-array1 = [1,None,2,3,None,None,5,None]
-
-def solution(array):
-    valid = 0            
-    res = []                 
-    for i in nums: 
-        if i is not None:    
+def algo_08(array):
+"""
+Given an array containing None values fill in the None values with most recent
+non None value in the array
+"""
+    valid = 0
+    res = []
+    for i in array:
+        if i is not None:
             res.append(i)
             valid = i
         else:
             res.append(valid)
     return res
 
-solution(array1)
 
-
-
-
-
-# ---------------------
-
-
-
-
-
-#Given two sentences, return an array that has the words that appear in one sentence and not
-#the other and an array with the words in common. 
-
-sentence1 = 'We are really pleased to meet you in our city'
-sentence2 = 'The city was hit by a really heavy storm'
-
-def solution(sentence1, sentence2):
+def algo_09(sentence1, sentence2):
+"""
+Given two sentences, return an array that has the words that appear in one sentence and not
+the other and an array with the words in common.
+"""
     set1 = set(sentence1.split())
     set2 = set(sentence2.split())
-    
-    return sorted(list(set1^set2)), sorted(list(set1&set2)) # ^ A.symmetric_difference(B), & A.intersection(B)
 
-print(solution(sentence1, sentence2))
+    return sorted(list(set1 ^ set2)), sorted(list(set1 & set2))  # ^ A.symmetric_difference(B), & A.intersection(B)
 
 
-
-
-
-
-# ---------------------
-
-
-
-
-
-
-
-# Given k numbers which are less than n, return the set of prime number among them
-# Note: The task is to write a program to print all Prime numbers in an Interval.
-# Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
-
-n = 35
-def solution(n):
+def algo_10(n):
+"""
+Given k numbers which are less than n, return the set of prime number among them
+Note: The task is to write a program to print all Prime numbers in an Interval.
+Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+"""
     prime_nums = []
     for num in range(n):
-        if num > 1: # all prime numbers are greater than 1
+        if num > 1: 
             for i in range(2, num):
-                if (num % i) == 0: # if the modulus == 0 is means that the number can be divided by a number preceding it
+                if (
+                        num % i) == 0:  # if the modulus == 0 is means that the number can be divided by a number preceding it
                     break
             else:
                 prime_nums.append(num)
     return prime_nums
-solution(n)
+
+
--- test/test_algo_work.py
+++ test/test_algo_work.py
@@ -1,10 +1,130 @@
-import unittest
+import pytest
+from src import algo_work
+import collections
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestOriginal:
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            [-231, -132],
+            [345, 543],
+        ]
+    )
+    def test_algo_01(self, input_value, expected_value):
+        assert algo_work.algo_01(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            ["Hi all, my name is Tom...I am originally from Australia.", 4.2],
+            ["I need to work very hard to learn more about algorithms in Python!", 4.08]
+        ]
+    )
+    def test_algo_02(self, input_value, expected_value):
+        assert algo_work.algo_02(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value_1', 'input_value_2', 'expected_value'],
+        argvalues=[
+            ['364', '1836', 2200]
+        ]
+    )
+    def test_algo_03a(self, input_value_1, input_value_2, expected_value):
+        assert algo_work.algo_03a(input_value_1, input_value_2) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value_1', 'input_value_2', 'expected_value'],
+        argvalues=[
+            ['364', '1836', 2200]
+        ]
+    )
+    def test_algo_03b(self, input_value_1, input_value_2, expected_value):
+        assert algo_work.algo_03b(input_value_1, input_value_2) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            ['alphabet', 1],
+            ['barbados', 2],
+            ['crunchy', 1],
+        ]
+    )
+    def test_algo_04a(self, input_value, expected_value):
+        assert algo_work.algo_04a(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            ['alphabet', 1],
+            ['barbados', 2],
+            ['crunchy', 1],
+        ]
+    )
+    def test_algo_04b(self, input_value, expected_value):
+        assert algo_work.algo_04b(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            ['radkar', True],
+        ]
+    )
+    def test_algo_05(self, input_value, expected_value):
+        assert algo_work.algo_05(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            [[6, 5, 4, 4], True],
+            [[1, 1, 1, 3, 3, 4, 3, 2, 4, 2], False],
+            [[1, 1, 2, 3, 7], True],
+        ]
+    )
+    def test_algo_06(self, input_value, expected_value):
+        assert algo_work.algo_06(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            [[0, 1, 0, 3, 12], [1, 3, 12, 0, 0]],
+            [[1, 7, 0, 0, 8, 0, 10, 12, 0, 4], [1, 7, 8, 10, 12, 4, 0, 0, 0, 0]],
+        ]
+    )
+    def test_algo_07(self, input_value, expected_value):
+        assert algo_work.algo_07(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value', 'expected_value'],
+        argvalues=[
+            [[1, None, 2, 3, None, None, 5, None], [1, 1, 2, 3, 3, 3, 5, 5]],
+        ]
+    )
+    def test_algo_08(self, input_value, expected_value):
+        assert algo_work.algo_08(input_value) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value_1','input_value_2', 'expected_value'],
+        argvalues=[
+            ['We are really pleased to meet you in our city', 'The city was hit by a really heavy storm', (['The','We','a','are','by','heavy','hit','in','meet','our','pleased','storm','to','was','you'],['city', 'really'])],
+        ]
+    )
+    def test_algo_09(self, input_value_1, input_value_2, expected_value):
+        assert algo_work.algo_09(input_value_1,input_value_2) == expected_value
+
+    @pytest.mark.parametrize(
+        argnames=['input_value_1', 'expected_value'],
+        argvalues=[
+            [35, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]],
+        ]
+    )
+    def test_algo_10(self, input_value, expected_value):
+        assert algo_work.algo_10(input_value) == expected_value
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestFailOnPurpose:
+
+    def test_algo_01_with_bad_input(self):
+        with pytest.raises(ValueError):
+            algo_work.algo_01('asdlkajsd')
